c9b823f20ac0519cd576afd84f96fc43f6644f39
ordohereticus <contact@mastermindsintl.com>
2023-06-21 06:11:51 -0400

    Revision 484

--- trunk/DarkMod/DarkModGlobals.cpp
+++ trunk/DarkMod/DarkModGlobals.cpp
@@ -9,12 +9,15 @@
  *
  * PROJECT: DarkMod
  * $Source$
- * $Revision: 483 $
- * $Date: 2006-07-13 02:27:20 -0400 (Thu, 13 Jul 2006) $
+ * $Revision: 484 $
+ * $Date: 2006-07-14 02:29:13 -0400 (Fri, 14 Jul 2006) $
  * $Author: ishtvan $
  * $Name$
  *
  * $Log$
+ * Revision 1.43  2006/07/14 06:29:13  ishtvan
+ * attempted surface type fix #2
+ *
  * Revision 1.42  2006/07/13 06:24:31  ishtvan
  * attempted surface type fix
  *
@@ -154,7 +157,7 @@
 
 #pragma warning(disable : 4996 4800)
 
-static bool init_version = FileVersionList("$Source$  $Revision: 483 $   $Date: 2006-07-13 02:27:20 -0400 (Thu, 13 Jul 2006) $", init_version);
+static bool init_version = FileVersionList("$Source$  $Revision: 484 $   $Date: 2006-07-14 02:29:13 -0400 (Fri, 14 Jul 2006) $", init_version);
 
 #ifdef _WINDOWS_
 #include "c:\compiled.h"
@@ -1227,7 +1230,7 @@ const char *DM_BuildOSPath(const char *basePath, const char *game, const char *r
 
 const char *CGlobal::GetSurfName(const idMaterial *material)
 {
-	int end;
+	int end = -1;
 	idStr returnStr = "none";
 	int surftype = material->GetSurfaceType();
 	if( surftype != SURFTYPE_15 )
@@ -1253,5 +1256,5 @@ Quit:
 
 	DM_LOG(LC_MISC, LT_DEBUG)LOGSTRING("Found new material name %s\r", returnStr.c_str());
 
-	return returnStr.c_str();
+	return va("%s", returnStr.c_str());
 }
