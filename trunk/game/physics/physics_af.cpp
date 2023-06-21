a800efabe4a826dbd178aee6f896d9f90ad65b00
ordohereticus <contact@mastermindsintl.com>
2023-06-21 14:22:31 -0400

    Revision 857

--- trunk/game/physics/physics_af.cpp
+++ trunk/game/physics/physics_af.cpp
@@ -2,9 +2,9 @@
  *
  * PROJECT: The Dark Mod
  * $Source$
- * $Revision: 807 $
- * $Date: 2007-02-28 16:58:45 -0500 (Wed, 28 Feb 2007) $
- * $Author: thelvyn $
+ * $Revision: 857 $
+ * $Date: 2007-03-21 04:59:53 -0400 (Wed, 21 Mar 2007) $
+ * $Author: ishtvan $
  *
  * $Log$
  * Revision 1.9  2006/08/07 06:53:46  ishtvan
@@ -41,7 +41,7 @@
 #include "../../idlib/precompiled.h"
 #pragma hdrstop
 
-static bool init_version = FileVersionList("$Source$  $Revision: 807 $   $Date: 2007-02-28 16:58:45 -0500 (Wed, 28 Feb 2007) $", init_version);
+static bool init_version = FileVersionList("$Source$  $Revision: 857 $   $Date: 2007-03-21 04:59:53 -0400 (Wed, 21 Mar 2007) $", init_version);
 
 #include "../Game_local.h"
 #include "../darkmod/playerdata.h"
@@ -6527,7 +6527,8 @@ const idBounds &idPhysics_AF::GetAbsBounds( int id ) const {
 idPhysics_AF::Evaluate
 ================
 */
-bool idPhysics_AF::Evaluate( int timeStepMSec, int endTimeMSec ) {
+bool idPhysics_AF::Evaluate( int timeStepMSec, int endTimeMSec ) 
+{
 	float timeStep;
 
 	if ( timeScaleRampStart < MS2SEC( endTimeMSec ) && timeScaleRampEnd > MS2SEC( endTimeMSec ) ) {
@@ -6567,6 +6568,26 @@ bool idPhysics_AF::Evaluate( int timeStepMSec, int endTimeMSec ) {
 
 	// move the af velocity into the frame of a pusher
 	AddPushVelocity( -current.pushVelocity );
+
+	// TDM: Enable the clipmodels of all team members for collisions
+	idEntity *part = NULL;
+	
+	idList<bool> InitClipStates;
+	bool PartClipState = false;
+
+	if( ((idAFEntity_Base *) self )->CollidesWithTeam() )
+	{
+		for ( part = self->GetTeamMaster(); part != NULL; part = part->GetNextTeamEntity() ) 
+		{
+			if ( part != self && part->GetPhysics() ) 
+			{
+				PartClipState = part->GetPhysics()->GetClipModel()->IsEnabled();
+				InitClipStates.Append( PartClipState );
+
+				part->GetPhysics()->EnableClip();
+			}
+		}
+	}
 
 #ifdef AF_TIMINGS
 	timer_total.Start();
@@ -6717,6 +6738,24 @@ bool idPhysics_AF::Evaluate( int timeStepMSec, int endTimeMSec ) {
 		timer_lcp.Clear();
 	}
 #endif
+
+
+	// TDM: Disable the clipmodels that we enabled above
+	int count = 0;
+
+	if( ((idAFEntity_Base *) self )->CollidesWithTeam() )
+	{
+		for ( part = self->GetTeamMaster(); part != NULL; part = part->GetNextTeamEntity() )  
+		{
+			if ( part != self && part->GetPhysics() ) 
+			{
+				if( !InitClipStates[count] )
+					part->GetPhysics()->DisableClip();
+
+				count++;
+			}
+		}
+	}
 
 	return true;
 }
