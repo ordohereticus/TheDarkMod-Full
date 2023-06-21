0e21d36fd8476599af3d430bb061c28fd69d4072
ordohereticus <contact@mastermindsintl.com>
2023-06-21 02:09:12 -0400

    Revision 272

---
+++ trunk/DarkMod/StimResponse.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************/
+/*                                                                            */
+/*         StimResponse (C) by Gerhard W. Gruber in Germany 2005              */
+/*                          All rights reserved                               */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************
+ *
+ * PROJECT: DarkMod
+ * $Source$
+ * $Revision: 272 $
+ * $Date: 2005-12-08 16:34:28 -0500 (Thu, 08 Dec 2005) $
+ * $Author: sparhawk $
+ * $Name$
+ *
+ * $Log$
+ * Revision 1.1  2005/12/08 21:34:28  sparhawk
+ * Intitial release
+ *
+ *
+ * DESCRIPTION: Implementation for the Stim/Response system.
+ *
+ *****************************************************************************/
+
+#include "../idlib/precompiled.h"
+#pragma hdrstop
+
+#include "StimResponse.h"
+
+CStimResponseCollection::CStimResponseCollection(void)
+{
+}
+
+CStimResponseCollection::~CStimResponseCollection(void)
+{
+}
+
