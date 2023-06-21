060a3785080e4810f2f075902c6c40e0a66ef3e8
ordohereticus <contact@mastermindsintl.com>
2023-06-21 04:33:19 -0400

    Revision 400

--- trunk/DarkMod/FrobDoor.h
+++ trunk/DarkMod/FrobDoor.h
@@ -2,11 +2,14 @@
  *
  * PROJECT: The Dark Mod
  * $Source$
- * $Revision: 399 $
- * $Date: 2006-05-02 16:39:33 -0400 (Tue, 02 May 2006) $
+ * $Revision: 400 $
+ * $Date: 2006-05-03 17:31:21 -0400 (Wed, 03 May 2006) $
  * $Author: sparhawk $
  *
  * $Log$
+ * Revision 1.11  2006/05/03 21:31:21  sparhawk
+ * Statechange callback script added.
+ *
  * Revision 1.10  2006/05/02 20:39:33  sparhawk
  * Translation added
  *
@@ -96,6 +99,17 @@ public:
 	void					DoneRotating( void );
 
 	/**
+	 *
+	 */
+	void					DoneMoving(void);
+
+	/**
+	 * A helper function that implements the finalisation for rotations or movings.
+	 */
+	void					DoneStateChange(void);
+	void					CallStateScript(void);
+
+	/**
 	* Find out if this door is touching another door, and if they share the same portal
 	* If so, store a pointer to the other door m_DoubleDoor on this door.
 	*
@@ -145,6 +159,8 @@ protected:
 	*	Useful for determining what to do when the door is stopped midway.
 	**/
 	bool						m_bIntentOpen;
+
+	bool						m_StateChange;
 
 	/**
 	* Set to true if the door was stopped mid-rotation
