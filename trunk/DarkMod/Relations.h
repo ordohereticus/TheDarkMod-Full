dea9450af99a67762a6c9dcd08db66584dee1bcb
ordohereticus <contact@mastermindsintl.com>
2023-06-21 00:01:14 -0400

    Revision 167

--- trunk/DarkMod/Relations.h
+++ trunk/DarkMod/Relations.h
@@ -31,12 +31,15 @@
  *
  * PROJECT: DarkMod
  * $Source$
- * $Revision: 96 $
- * $Date: 2005-03-30 13:16:20 -0500 (Wed, 30 Mar 2005) $
- * $Author: sparhawk $
+ * $Revision: 167 $
+ * $Date: 2005-09-25 21:12:21 -0400 (Sun, 25 Sep 2005) $
+ * $Author: ishtvan $
  * $Name$
  *
  * $Log$
+ * Revision 1.3  2005/09/26 01:12:21  ishtvan
+ * no longer tries to access relationship matrix when loading it has failed
+ *
  * Revision 1.2  2005/03/30 18:16:20  sparhawk
  * CVS Header added
  *
@@ -163,6 +166,12 @@ protected:
 * The relationship matrix uses class CMatrixSq to store a square matrix
 **/
 	CMatrixSq<int> *	m_RelMat;
+
+/**
+* Boolean to store whether the relations matrix failed to load
+* Accessors will check this and return the default relation if true
+**/
+	bool				m_bMatFailed;
 
 };
 
