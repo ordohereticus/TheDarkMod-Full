bb141e0aa35aab5320813b75f41803fffaa8ba1d
ordohereticus <contact@mastermindsintl.com>
2023-06-22 16:40:53 -0400

    Revision 2134

--- trunk/game/player.cpp
+++ trunk/game/player.cpp
@@ -1,9 +1,9 @@
 /***************************************************************************
  *
  * PROJECT: The Dark Mod
- * $Revision: 2066 $
- * $Date: 2008-02-09 02:27:06 -0500 (Sat, 09 Feb 2008) $
- * $Author: greebo $
+ * $Revision: 2134 $
+ * $Date: 2008-03-20 06:47:16 -0400 (Thu, 20 Mar 2008) $
+ * $Author: crispy $
  *
  ***************************************************************************/
 // Copyright (C) 2004 Id Software, Inc.
@@ -14,7 +14,7 @@
 
 #pragma warning(disable : 4355) // greebo: Disable warning "'this' used in constructor"
 
-static bool init_version = FileVersionList("$Id: player.cpp 2066 2008-02-09 07:27:06Z greebo $", init_version);
+static bool init_version = FileVersionList("$Id: player.cpp 2134 2008-03-20 10:47:16Z crispy $", init_version);
 
 #include "game_local.h"
 #include "../DarkMod/DarkModGlobals.h"
@@ -3951,6 +3951,11 @@ void idPlayer::BobCycle( const idVec3 &pushVelocity ) {
 		bobFoot = 0;
 		bobfracsin = 0;
 	} else if ( ( !usercmd.forwardmove && !usercmd.rightmove ) || ( xyspeed <= MIN_BOB_SPEED ) ) {
+		// Play a footstep sound when we stop walking (the foot is lowered to the ground;
+		// also this prevents exploits)
+		if (bobCycle != 0) {
+			PlayFootStepSound();
+		}
 		// start at beginning of cycle again
 		bobCycle = 0;
 		bobFoot = 0;
@@ -3964,11 +3969,25 @@ void idPlayer::BobCycle( const idVec3 &pushVelocity ) {
 			bobmove = pm_walkbob.GetFloat() * ( 1.0f - bobFrac ) + pm_runbob.GetFloat() * bobFrac;
 		}
 
-		// check for footstep / splash sounds
+		// check for footstep / splash sounds <--- old D3 comment, probably legacy
+		// additional explanatory comments added by Crispy
 		old = bobCycle;
+		// bobCycle is effectively an 8-bit integer, which increases at a speed determined by bobmove,
+		// and wraps around when it exceeds 8 bits.
 		bobCycle = (int)( old + bobmove * gameLocal.msec ) & 255;
+		// bobFoot = most significant bit of bobCycle, so it will be equal to 1 for half the time,
+		// and 0 for the other half. This represents which foot we're placing our weight on right now.
 		bobFoot = ( bobCycle & 128 ) >> 7;
+		// Take the other 7 bits of bobCycle, scale them to range from 0 to PI, and take the sine.
+		// The result produces positive values only, from within the first "hump" of the function.
+		// (Look at the graph of sin(x) with x = 0...PI)
 		bobfracsin = idMath::Fabs( sin( ( bobCycle & 127 ) / 127.0 * idMath::PI ) );
+		
+		// Crispy: Play footstep sounds when we hit the bottom of the cycle (i.e. when bobFoot changes)
+		if ((old&128) != (bobCycle&128)) {
+			// We've changed feet, so play a footstep
+			PlayFootStepSound();
+		}
 	}
 
 	// calculate angles for view bobbing
