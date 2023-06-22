d0513652a011a8870071ec9f9189fd897e3af6d5
ordohereticus <contact@mastermindsintl.com>
2023-06-22 03:02:53 -0400

    Revision 1498

--- branches/AI_greebo/game/ai/ai.h
+++ branches/AI_greebo/game/ai/ai.h
@@ -1,8 +1,8 @@
 /***************************************************************************
  *
  * PROJECT: The Dark Mod
- * $Revision: 1491 $
- * $Date: 2007-10-20 09:41:12 -0400 (Sat, 20 Oct 2007) $
+ * $Revision: 1498 $
+ * $Date: 2007-10-20 17:00:14 -0400 (Sat, 20 Oct 2007) $
  * $Author: greebo $
  *
  ***************************************************************************/
@@ -1141,7 +1141,7 @@ public: // greebo: Made these public for now, I didn't want to write an accessor
 	* The point of this function is to determine the visual stimulus level caused
 	* by the players current lightgem value.
 	*/
-	float getPlayerVisualStimulusAmount(idEntity* p_playerEntity) const;
+	float GetPlayerVisualStimulusAmount(idEntity* p_playerEntity) const;
 
 
 	// attacks
