6e01abeb9c01d0db040a7387dd79cd1626d7dc39
ordohereticus <contact@mastermindsintl.com>
2023-06-22 19:19:22 -0400

    Revision 2257

--- trunk/game/ai/ai.h
+++ trunk/game/ai/ai.h
@@ -1,9 +1,9 @@
 /***************************************************************************
  *
  * PROJECT: The Dark Mod
- * $Revision: 2247 $
- * $Date: 2008-04-29 16:21:37 -0400 (Tue, 29 Apr 2008) $
- * $Author: angua $
+ * $Revision: 2257 $
+ * $Date: 2008-05-01 09:15:35 -0400 (Thu, 01 May 2008) $
+ * $Author: greebo $
  *
  ***************************************************************************/
 
@@ -608,10 +608,6 @@ protected:
 	idStr					attack;
 
 	// chatter/talking
-	const idSoundShader		*chat_snd;
-	int						chat_min;
-	int						chat_max;
-	int						chat_time; // The next time chattering is allowed
 	talkState_t				talk_state;
 	idEntityPtr<idActor>	talkTarget;
 
@@ -1033,7 +1029,6 @@ public: // greebo: Made these public for now, I didn't want to write an accessor
 	int						ReactionTo( const idEntity *ent );
 	bool					CheckForEnemy( void );
 	void					EnemyDead( void );
-	virtual bool			CanPlayChatterSounds( void ) const;
 
 	
 	/** 
@@ -1064,13 +1059,6 @@ public: // greebo: Made these public for now, I didn't want to write an accessor
 	// the last time where the AI did its thinking (used for physics)
 	int						m_lastThinkTime;
 
-	/**
-	 * greebo: Sets the chatter sound depending on having an enemy or not. The chat_time
-	 * member is updated as well, but the sound is not actually played, it only paves the way.
-	 */
-	void					SetChatSound();
-
-	void					PlayChatter( void );
 	virtual void			Hide( void );
 	virtual void			Show( void );
 	virtual bool			CanBecomeSolid();
