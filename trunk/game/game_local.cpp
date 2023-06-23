663390d58feb2978d12fae7bfe296d529fbac413
ordohereticus <contact@mastermindsintl.com>
2023-06-23 02:46:12 -0400

    Revision 2580

--- trunk/game/game_local.cpp
+++ trunk/game/game_local.cpp
@@ -1,8 +1,8 @@
 /***************************************************************************
  *
  * PROJECT: The Dark Mod
- * $Revision: 2579 $
- * $Date: 2008-06-26 16:12:28 -0400 (Thu, 26 Jun 2008) $
+ * $Revision: 2580 $
+ * $Date: 2008-06-27 00:01:19 -0400 (Fri, 27 Jun 2008) $
  * $Author: greebo $
  *
  ***************************************************************************/
@@ -15,7 +15,7 @@
 
 #pragma warning(disable : 4127 4996 4805 4800)
 
-static bool init_version = FileVersionList("$Id: game_local.cpp 2579 2008-06-26 20:12:28Z greebo $", init_version);
+static bool init_version = FileVersionList("$Id: game_local.cpp 2580 2008-06-27 04:01:19Z greebo $", init_version);
 
 #include "game_local.h"
 #include "../DarkMod/DarkModGlobals.h"
@@ -5899,10 +5899,6 @@ void idGameLocal::ProcessStimResponse(unsigned long ticks)
 					}
 
 					bounds.ExpandSelf(radius);
-				}
-
-				if (stim->m_StimTypeId == ST_GAS) {
-					gameRenderWorld->DebugBox(colorGreen, idBox(bounds), 48);
 				}
 
 				// Collision-based stims
