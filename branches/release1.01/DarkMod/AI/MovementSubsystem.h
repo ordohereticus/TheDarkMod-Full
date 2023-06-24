10010fe7fb18aa87bbd61d5da20168ab2d45e934
ordohereticus <contact@mastermindsintl.com>
2023-06-24 14:38:11 -0400

    Revision 3832

---
+++ branches/release1.01/DarkMod/AI/MovementSubsystem.h
@@ -0,0 +1,100 @@
+/***************************************************************************
+ *
+ * PROJECT: The Dark Mod
+ * $Revision: 3767 $
+ * $Date: 2009-12-01 00:17:07 -0500 (Tue, 01 Dec 2009) $
+ * $Author: angua $
+ *
+ ***************************************************************************/
+
+#ifndef __AI_MOVEMENTSUBSYSTEM_H__
+#define __AI_MOVEMENTSUBSYSTEM_H__
+
+#include <boost/shared_ptr.hpp>
+#include <list>
+
+#include "Tasks/Task.h"
+#include "Subsystem.h"
+#include "Queue.h"
+
+namespace ai
+{
+
+class MovementSubsystem :
+	public Subsystem 
+{
+public:
+	enum BlockedState
+	{
+		ENotBlocked,		// moving along
+		EPossiblyBlocked,	// might be blocked, watching...
+		EBlocked,			// not been making progress for too long
+		EResolvingBlock,	// resolving block
+	};
+
+protected:
+	bool _patrolling;
+
+	// The origin history, contains the origin position of the last few frames
+	idList<idVec3> _originHistory;
+
+	// Currently active list index
+	int _curHistoryIndex;
+
+	// These bounds contain all origin locations of the last few frames
+	idBounds _historyBounds;
+
+	// Mininum radius the bounds need to have, otherwise state is raised to EPossiblyBlocked
+	float _historyBoundsThreshold;
+
+	BlockedState _state;
+
+	int _lastTimeNotBlocked;
+
+	// The amount of time allowed to pass during EPossiblyBlocked before state is set to EBlocked 
+	int _blockTimeOut;
+
+public:
+	MovementSubsystem(SubsystemId subsystemId, idAI* owner);
+
+	// Called regularly by the Mind to run the currently assigned routine.
+	// @returns: TRUE if the subsystem is enabled and the task was performed, 
+	// @returns: FALSE if the subsystem is disabled and nothing happened.
+	virtual bool PerformTask();
+
+	void StartPatrol();
+	void Patrol();
+
+	virtual void StartPathTask();
+	virtual void NextPath();
+
+	virtual void ClearTasks();
+
+
+	// Save/Restore methods
+	virtual void Save(idSaveGame* savefile) const;
+	virtual void Restore(idRestoreGame* savefile);
+
+	// Returns the current "blocked" state
+	BlockedState GetBlockedState() const
+	{
+		return _state;
+	}
+
+	void SetBlockedState(const BlockedState newState);
+
+	void ResolveBlock(idEntity* blockingEnt);
+
+	bool IsResolvingBlock();
+
+protected:
+	virtual void CheckBlocked(idAI* owner);
+
+private:
+	void DebugDraw(idAI* owner);
+};
+typedef boost::shared_ptr<MovementSubsystem> MovementSubsystemPtr;
+
+} // namespace ai
+
+#endif /* __AI_MOVEMENTSUBSYSTEM_H__ */
