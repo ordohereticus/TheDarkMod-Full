2f3fd635418ef0b41dd5b24bd2eafe2354695a19
ordohereticus <contact@mastermindsintl.com>
2023-06-24 09:37:49 -0400

    Revision 3691

--- trunk/game/player.cpp
+++ trunk/game/player.cpp
@@ -1,8 +1,8 @@
 /***************************************************************************
  *
  * PROJECT: The Dark Mod
- * $Revision: 3685 $
- * $Date: 2009-09-01 08:16:10 -0400 (Tue, 01 Sep 2009) $
+ * $Revision: 3691 $
+ * $Date: 2009-09-05 04:13:07 -0400 (Sat, 05 Sep 2009) $
  * $Author: greebo $
  *
  ***************************************************************************/
@@ -14,7 +14,7 @@
 
 #pragma warning(disable : 4355) // greebo: Disable warning "'this' used in constructor"
 
-static bool init_version = FileVersionList("$Id: player.cpp 3685 2009-09-01 12:16:10Z greebo $", init_version);
+static bool init_version = FileVersionList("$Id: player.cpp 3691 2009-09-05 08:13:07Z greebo $", init_version);
 
 #include "game_local.h"
 #include "ai/aas_local.h"
@@ -10776,7 +10776,7 @@ void idPlayer::PlayFootStepSound()
 
 		if (sound.IsEmpty())
 		{
-			gameLocal.Warning("Did not find footstep sound %s", (localSound + moveType).c_str());
+			//gameLocal.Warning("Did not find footstep sound %s", (localSound + moveType).c_str());
 
 			// Fall back to normal sound
 			sound = spawnArgs.GetString( localSound );
