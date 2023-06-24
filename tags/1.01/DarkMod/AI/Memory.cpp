be55dca3b23bfd45dfc83b9cd0201362826238c9
ordohereticus <contact@mastermindsintl.com>
2023-06-24 15:11:18 -0400

    Revision 3844

---
+++ tags/1.01/DarkMod/AI/Memory.cpp
@@ -0,0 +1,309 @@
+/***************************************************************************
+ *
+ * PROJECT: The Dark Mod
+ * $Revision: 3689 $
+ * $Date: 2009-09-03 08:40:05 -0400 (Thu, 03 Sep 2009) $
+ * $Author: greebo $
+ *
+ ***************************************************************************/
+
+#include "../idlib/precompiled.h"
+#pragma hdrstop
+
+static bool init_version = FileVersionList("$Id: Memory.cpp 3689 2009-09-03 12:40:05Z greebo $", init_version);
+
+#include "Memory.h"
+#include "../../game/ai/ai.h"
+
+namespace ai
+{
+
+Memory::Memory(idAI* owningAI) :
+	owner(owningAI),
+	alertState(ERelaxed),
+	lastAlertRiseTime(-1),
+	lastPatrolChatTime(-1),
+	lastTimeFriendlyAISeen(-1000),
+	lastTimeEnemySeen(-1),
+	lastTimeVisualStimBark(-1),
+	countEvidenceOfIntruders(0),
+	nextHeadTurnCheckTime(0),
+	currentlyHeadTurning(false),
+	headTurnEndTime(0),
+	idlePosition(idMath::INFINITY, idMath::INFINITY, idMath::INFINITY),
+	idleYaw(0),
+	playIdleAnimations(true),
+	enemiesHaveBeenSeen(false),
+	hasBeenAttackedByEnemy(false),
+	itemsHaveBeenStolen(false),
+	itemsHaveBeenBroken(false),
+	unconsciousPeopleHaveBeenFound(false),
+	deadPeopleHaveBeenFound(false),
+	alertPos(0,0,0),
+	alertClass(EAlertClassCount),
+	alertType(EAlertTypeCount),
+	alertRadius(-1),
+	stimulusLocationItselfShouldBeSearched(false),
+	investigateStimulusLocationClosely(false),
+	alertedDueToCommunication(false),
+	lastAlertPosSearched(0,0,0),
+	alertSearchCenter(idMath::INFINITY, idMath::INFINITY, idMath::INFINITY),
+	alertSearchVolume(0,0,0),
+	alertSearchExclusionVolume(0,0,0),
+	lastEnemyPos(0,0,0),
+	canHitEnemy(false),
+	willBeAbleToHitEnemy(false),
+	canBeHitByEnemy(false),
+	currentSearchSpot(0,0,0),
+	hidingSpotTestStarted(false),
+	hidingSpotSearchDone(true),
+	noMoreHidingSpots(false),
+	restartSearchForHidingSpots(false),
+	hidingSpotThinkFrameCount(0),
+	firstChosenHidingSpotIndex(0),
+	currentChosenHidingSpotIndex(0),
+	chosenHidingSpot(0,0,0),
+	hidingSpotInvestigationInProgress(false),
+	fleeingDone(true),
+	positionBeforeTakingCover(0,0,0),
+	resolvingMovementBlock(false)
+{}
+
+// Save/Restore routines
+void Memory::Save(idSaveGame* savefile) const
+{
+	savefile->WriteObject(owner);
+	savefile->WriteInt(static_cast<int>(alertState));
+	currentPath.Save(savefile);
+	nextPath.Save(savefile);
+	lastPath.Save(savefile);
+	savefile->WriteInt(lastAlertRiseTime);
+	savefile->WriteInt(lastPatrolChatTime);
+	savefile->WriteInt(lastTimeFriendlyAISeen);
+	savefile->WriteInt(lastTimeEnemySeen);
+	savefile->WriteInt(lastTimeVisualStimBark);
+	savefile->WriteInt(countEvidenceOfIntruders);
+	savefile->WriteInt(nextHeadTurnCheckTime);
+	savefile->WriteBool(currentlyHeadTurning);
+	savefile->WriteInt(headTurnEndTime);
+	savefile->WriteVec3(idlePosition);
+	savefile->WriteFloat(idleYaw);
+	savefile->WriteBool(playIdleAnimations);
+	savefile->WriteBool(enemiesHaveBeenSeen);
+	savefile->WriteBool(hasBeenAttackedByEnemy);
+	savefile->WriteBool(itemsHaveBeenStolen);
+	savefile->WriteBool(itemsHaveBeenBroken);
+	savefile->WriteBool(unconsciousPeopleHaveBeenFound);
+	savefile->WriteBool(deadPeopleHaveBeenFound);
+	savefile->WriteVec3(alertPos);
+	savefile->WriteInt(static_cast<int>(alertClass));
+	savefile->WriteInt(static_cast<int>(alertType));
+	savefile->WriteFloat(alertRadius);
+	savefile->WriteBool(stimulusLocationItselfShouldBeSearched);
+	savefile->WriteBool(investigateStimulusLocationClosely);
+	savefile->WriteBool(alertedDueToCommunication);
+	savefile->WriteVec3(lastAlertPosSearched);
+	savefile->WriteVec3(alertSearchCenter);
+	savefile->WriteVec3(alertSearchVolume);
+	savefile->WriteVec3(alertSearchExclusionVolume);
+	savefile->WriteVec3(lastEnemyPos);
+	savefile->WriteBool(canHitEnemy);
+	savefile->WriteBool(willBeAbleToHitEnemy);
+	savefile->WriteBool(canBeHitByEnemy);
+	savefile->WriteVec3(currentSearchSpot);
+	savefile->WriteBool(hidingSpotTestStarted);
+	savefile->WriteBool(hidingSpotSearchDone);
+	savefile->WriteBool(noMoreHidingSpots);
+	savefile->WriteBool(restartSearchForHidingSpots);
+	savefile->WriteInt(hidingSpotThinkFrameCount);
+	savefile->WriteInt(firstChosenHidingSpotIndex);
+	savefile->WriteInt(currentChosenHidingSpotIndex);
+	savefile->WriteVec3(chosenHidingSpot);
+	savefile->WriteBool(hidingSpotInvestigationInProgress);
+	savefile->WriteBool(fleeingDone);
+	savefile->WriteVec3(positionBeforeTakingCover);
+	savefile->WriteBool(resolvingMovementBlock);
+
+	doorRelated.currentDoor.Save(savefile);
+
+	savefile->WriteInt(doorRelated.doorInfo.size());
+	for (DoorInfoMap::const_iterator i = doorRelated.doorInfo.begin();
+		 i != doorRelated.doorInfo.end(); ++i)
+	{
+		savefile->WriteObject(i->first);
+		i->second->Save(savefile);
+	}
+
+	// greebo: Don't save the AAS areaNum => DoorInfo mapping (can be re-calculated at restore time)
+
+	savefile->WriteInt(static_cast<int>(greetingInfo.size()));
+
+	for (ActorGreetingInfoMap::const_iterator i = greetingInfo.begin();
+		 i != greetingInfo.end(); ++i)
+	{
+		savefile->WriteObject(i->first);
+		savefile->WriteInt(i->second.lastGreetingTime);
+		savefile->WriteInt(i->second.lastConsiderTime);
+	}
+}
+
+void Memory::Restore(idRestoreGame* savefile)
+{
+	savefile->ReadObject(reinterpret_cast<idClass*&>(owner));
+
+	int temp;
+	savefile->ReadInt(temp);
+	alertState = static_cast<EAlertState>(temp);
+
+	currentPath.Restore(savefile);
+	nextPath.Restore(savefile);
+	lastPath.Restore(savefile);
+	savefile->ReadInt(lastAlertRiseTime);
+	savefile->ReadInt(lastPatrolChatTime);
+	savefile->ReadInt(lastTimeFriendlyAISeen);
+	savefile->ReadInt(lastTimeEnemySeen);
+	savefile->ReadInt(lastTimeVisualStimBark);
+	savefile->ReadInt(countEvidenceOfIntruders);
+	savefile->ReadInt(nextHeadTurnCheckTime);
+	savefile->ReadBool(currentlyHeadTurning);
+	savefile->ReadInt(headTurnEndTime);
+	savefile->ReadVec3(idlePosition);
+	savefile->ReadFloat(idleYaw);
+	savefile->ReadBool(playIdleAnimations);
+	savefile->ReadBool(enemiesHaveBeenSeen);
+	savefile->ReadBool(hasBeenAttackedByEnemy);
+	savefile->ReadBool(itemsHaveBeenStolen);
+	savefile->ReadBool(itemsHaveBeenBroken);
+	savefile->ReadBool(unconsciousPeopleHaveBeenFound);
+	savefile->ReadBool(deadPeopleHaveBeenFound);
+	savefile->ReadVec3(alertPos);
+
+	savefile->ReadInt(temp);
+	alertClass = static_cast<EAlertClass>(temp);
+
+	savefile->ReadInt(temp);
+	alertType = static_cast<EAlertType>(temp);
+
+	savefile->ReadFloat(alertRadius);
+	savefile->ReadBool(stimulusLocationItselfShouldBeSearched);
+	savefile->ReadBool(investigateStimulusLocationClosely);
+	savefile->ReadBool(alertedDueToCommunication);
+	savefile->ReadVec3(lastAlertPosSearched);
+	savefile->ReadVec3(alertSearchCenter);
+	savefile->ReadVec3(alertSearchVolume);
+	savefile->ReadVec3(alertSearchExclusionVolume);
+	savefile->ReadVec3(lastEnemyPos);
+	savefile->ReadBool(canHitEnemy);
+	savefile->ReadBool(willBeAbleToHitEnemy);
+	savefile->ReadBool(canBeHitByEnemy);
+	savefile->ReadVec3(currentSearchSpot);
+	savefile->ReadBool(hidingSpotTestStarted);
+	savefile->ReadBool(hidingSpotSearchDone);
+	savefile->ReadBool(noMoreHidingSpots);
+	savefile->ReadBool(restartSearchForHidingSpots);
+	savefile->ReadInt(hidingSpotThinkFrameCount);
+	savefile->ReadInt(firstChosenHidingSpotIndex);
+	savefile->ReadInt(currentChosenHidingSpotIndex);
+	savefile->ReadVec3(chosenHidingSpot);
+	savefile->ReadBool(hidingSpotInvestigationInProgress);
+	savefile->ReadBool(fleeingDone);
+	savefile->ReadVec3(positionBeforeTakingCover);
+	savefile->ReadBool(resolvingMovementBlock);
+
+	doorRelated.currentDoor.Restore(savefile);
+	// Clear the containers before restoring them
+	doorRelated.doorInfo.clear();
+	doorRelated.areaDoorInfoMap.clear();
+
+	int num;
+	savefile->ReadInt(num);
+	for (int i = 0; i < num; i++)
+	{
+		CFrobDoor* door;
+		savefile->ReadObject(reinterpret_cast<idClass*&>(door));
+
+		// Allocate a new doorinfo structure and insert it into the map
+		DoorInfoPtr info(new DoorInfo);
+
+		std::pair<DoorInfoMap::iterator, bool> result = 
+			doorRelated.doorInfo.insert(DoorInfoMap::value_type(door, info));
+
+		// The insertion must succeed (unique pointers in the map!), otherwise we have inconsistent data
+		assert(result.second == true);
+
+		info->Restore(savefile);
+	}
+
+	// greebo: Reconstruct the AAS areaNum => DoorInfo mapping
+	for (DoorInfoMap::iterator i = doorRelated.doorInfo.begin();
+		 i != doorRelated.doorInfo.end(); ++i)
+	{
+		// Use the areanumber as index and insert the pointer into the map
+		doorRelated.areaDoorInfoMap.insert(
+			AreaToDoorInfoMap::value_type(i->second->areaNum, i->second)
+		);
+	}
+
+	greetingInfo.clear();
+
+	savefile->ReadInt(temp);
+	for (int i = 0; i < temp; ++i)
+	{
+		idAI* ai;
+		savefile->ReadObject(reinterpret_cast<idClass*&>(ai));
+		
+		std::pair<ActorGreetingInfoMap::iterator, bool> result = greetingInfo.insert(
+			ActorGreetingInfoMap::value_type(ai, GreetingInfo()));
+		
+		savefile->ReadInt(result.first->second.lastGreetingTime);
+		savefile->ReadInt(result.first->second.lastConsiderTime);
+	}
+}
+
+DoorInfo& Memory::GetDoorInfo(CFrobDoor* door)
+{
+	DoorInfoMap::iterator i = doorRelated.doorInfo.find(door);
+
+	if (i != doorRelated.doorInfo.end())
+	{
+		return *(i->second);
+	}
+	else
+	{
+		DoorInfoPtr info(new DoorInfo);
+		// Set the area number
+		info->areaNum = door->GetAASArea(owner->GetAAS());
+
+		// Insert into the map
+		std::pair<DoorInfoMap::iterator, bool> result =
+			doorRelated.doorInfo.insert(DoorInfoMap::value_type(door, info));
+
+		// Add the areaNum => info mapping for faster lookup using area numbers
+		doorRelated.areaDoorInfoMap.insert(
+			AreaToDoorInfoMap::value_type(info->areaNum, info)
+		);
+
+		return *(result.first->second);
+	}
+}
+
+DoorInfoPtr Memory::GetDoorInfo(int areaNum)
+{
+	AreaToDoorInfoMap::iterator i = doorRelated.areaDoorInfoMap.find(areaNum);
+	return (i != doorRelated.areaDoorInfoMap.end()) ? i->second : DoorInfoPtr();
+}
+
+Memory::GreetingInfo& Memory::GetGreetingInfo(idActor* actor)
+{
+	// Insert structure if not existing
+	ActorGreetingInfoMap::iterator i = greetingInfo.find(actor);
+
+	if (i == greetingInfo.end())
+	{
+		i = greetingInfo.insert(ActorGreetingInfoMap::value_type(actor, GreetingInfo())).first;
+	}
+
+	return i->second;
+}
+
+} // namespace ai
---
+++ tags/1.01/DarkMod/AI/States/DeadState.cpp
@@ -0,0 +1,105 @@
+/***************************************************************************
+ *
+ * PROJECT: The Dark Mod
+ * $Revision: 3705 $
+ * $Date: 2009-09-15 05:58:24 -0400 (Tue, 15 Sep 2009) $
+ * $Author: angua $
+ *
+ ***************************************************************************/
+
+#include "../idlib/precompiled.h"
+#pragma hdrstop
+
+static bool init_version = FileVersionList("$Id: DeadState.cpp 3705 2009-09-15 09:58:24Z angua $", init_version);
+
+#include "DeadState.h"
+#include "../Memory.h"
+#include "../Library.h"
+
+namespace ai
+{
+
+// Get the name of this state
+const idStr& DeadState::GetName() const
+{
+	static idStr _name(STATE_DEAD);
+	return _name;
+}
+
+void DeadState::Init(idAI* owner)
+{
+	// Init base class first
+	State::Init(owner);
+
+	DM_LOG(LC_AI, LT_INFO)LOGSTRING("DeadState initialised.\r");
+	assert(owner);
+
+	// Stop move!
+	owner->StopMove(MOVE_STATUS_DONE);
+
+	owner->StopAnim(ANIMCHANNEL_TORSO, 0);
+	owner->StopAnim(ANIMCHANNEL_LEGS, 0);
+	owner->StopAnim(ANIMCHANNEL_HEAD, 0);
+
+/*
+// angua: disabled for Thief's den release
+// anims didn't look good and produced problems
+	owner->SetAnimState(ANIMCHANNEL_TORSO, "Torso_Death", 0);
+	owner->SetAnimState(ANIMCHANNEL_LEGS, "Legs_Death", 0);
+*/
+	owner->SetAnimState(ANIMCHANNEL_HEAD, "Head_Death", 0);
+
+	// greebo: Set the waitstate, this gets cleared by 
+	// the script function when the animation is done.
+	owner->SetWaitState(ANIMCHANNEL_HEAD, "death");
+
+	// Don't do anything else, the death animation will finish in a few frames
+	// and the AI is done afterwards.
+
+	// Clear all the subsystems
+	owner->movementSubsystem->ClearTasks();
+	owner->senseSubsystem->ClearTasks();
+	owner->actionSubsystem->ClearTasks();
+	owner->commSubsystem->ClearTasks();
+
+	_waitingForDeath = true;
+}
+
+// Gets called each time the mind is thinking
+void DeadState::Think(idAI* owner)
+{
+	if (_waitingForDeath 
+		&&	idStr(owner->WaitState(ANIMCHANNEL_TORSO)) != "death"
+		&&	idStr(owner->WaitState(ANIMCHANNEL_LEGS)) != "death"
+		&&	idStr(owner->WaitState(ANIMCHANNEL_HEAD)) != "death") 
+	{
+		owner->PostDeath();
+		_waitingForDeath = false;
+	}
+}
+
+// Save/Restore methods
+void DeadState::Save(idSaveGame* savefile) const
+{
+	State::Save(savefile);
+	savefile->WriteBool(_waitingForDeath);
+}
+
+void DeadState::Restore(idRestoreGame* savefile) 
+{
+	State::Restore(savefile);
+	savefile->ReadBool(_waitingForDeath);
+}
+
+StatePtr DeadState::CreateInstance()
+{
+	return StatePtr(new DeadState);
+}
+
+// Register this state with the StateLibrary
+StateLibrary::Registrar deadStateRegistrar(
+	STATE_DEAD, // Task Name
+	StateLibrary::CreateInstanceFunc(&DeadState::CreateInstance) // Instance creation callback
+);
+
+} // namespace ai
