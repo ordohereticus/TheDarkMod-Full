47a28fcfee28237e1cfbce7acc84cc08a861fb99
ordohereticus <contact@mastermindsintl.com>
2023-06-25 17:27:32 -0400

    Revision 4408

--- trunk/DarkMod/sourcehook/FastDelegate.h
+++ trunk/DarkMod/sourcehook/FastDelegate.h
@@ -1,12 +1,3 @@
-/***************************************************************************
- *
- * PROJECT: The Dark Mod
- * $Revision: 1435 $
- * $Date: 2007-10-16 12:53:28 -0400 (Tue, 16 Oct 2007) $
- * $Author: greebo $
- *
- ***************************************************************************/
-
 //						FastDelegate.h
 //	Efficient delegates in C++ that generate only two lines of asm code!
 //  Documentation is found at http://www.codeproject.com/cpp/FastDelegate.asp
@@ -3818,4 +3809,3 @@ FastDelegate20<Param1, Param2, Param3, Param4, Param5, Param6, Param7, Param8, P
 } // namespace fastdelegate
 
 #endif // !defined(FASTDELEGATE_H)
-
