fca6276bf67b9c84d93ba833b7d9305a5bc683f6
ordohereticus <contact@mastermindsintl.com>
2023-06-26 02:20:44 -0400

    Revision 4581

--- trunk/DarkMod/SEED.cpp
+++ trunk/DarkMod/SEED.cpp
@@ -2,8 +2,8 @@
 /***************************************************************************
  *
  * PROJECT: The Dark Mod
- * $Revision: 4579 $
- * $Date: 2011-02-11 04:32:08 -0500 (Fri, 11 Feb 2011) $
+ * $Revision: 4581 $
+ * $Date: 2011-02-11 05:24:43 -0500 (Fri, 11 Feb 2011) $
  * $Author: tels $
  *
  ***************************************************************************/
@@ -66,7 +66,7 @@ TODO: We currently determine the material by doing a point-trace, then when the 
 // define to output debug info about watched and combined entities
 //#define M_DEBUG_COMBINE
 
-static bool init_version = FileVersionList("$Id: SEED.cpp 4579 2011-02-11 09:32:08Z tels $", init_version);
+static bool init_version = FileVersionList("$Id: SEED.cpp 4581 2011-02-11 10:24:43Z tels $", init_version);
 
 #include "SEED.h"
 
@@ -156,7 +156,7 @@ Seed::~Seed
 */
 Seed::~Seed(void) {
 
-	//gameLocal.Warning ("SEED %s: Shutdown.\n", GetName() );
+	// gameLocal.Warning ("SEED %s: Shutdown.\n", GetName() );
 	ClearClasses();
 }
 /*
@@ -426,7 +426,11 @@ void Seed::ClearClasses( void )
 	int n = m_Classes.Num();
 	for(int i = 0; i < n; i++ )
 	{
-		// no need to free these as they are just ptrs
+		// need to free this?
+		if (!m_Classes[i].pseudo && m_Classes[i].hModel)
+		{
+			renderModelManager->FreeModel( m_Classes[i].hModel );
+		}
 		m_Classes[i].hModel = NULL;
 		if (m_Classes[i].imgmap != 0)
 		{
@@ -1564,10 +1568,8 @@ void Seed::AddClassFromEntity( idEntity *ent, const bool watch, const bool getSp
 		// check if this is a func_static with a model, or an "inline map geometry" func static
 		if (SeedClass.modelname == ent->GetName())
 		{
-			// simply point to the already existing model, so we can recover the into-the-map-inlined geometry:
-			SeedClass.hModel = ent->GetRenderEntity()->hModel;
-			// prevent a double free
-			ent->GetRenderEntity()->hModel = NULL;
+			// Make a copy of the already existing model, so we can safely free it anytime:
+			SeedClass.hModel = gameLocal.m_ModelGenerator->DuplicateModel( ent->GetRenderEntity()->hModel, SeedClass.classname, true );
 			// set a dummy model
 			SeedClass.modelname = "models/darkmod/junk/plank_short.lwo";
 
@@ -1588,8 +1590,8 @@ void Seed::AddClassFromEntity( idEntity *ent, const bool watch, const bool getSp
 			   		(SeedClass.scale_min.x != 0.0f && SeedClass.scale_min.x != 1.0f) ||
 			   		(SeedClass.scale_max.x != 1.0f || SeedClass.scale_min.y != 1.0f || SeedClass.scale_max.y != 1.0f) )
 				{
-				// simply point to the already existing model, so we can clone it later
-				SeedClass.hModel = ent->GetRenderEntity()->hModel;
+				// Make a copy of the already existing model, so we can safely free it anytime:
+				SeedClass.hModel = gameLocal.m_ModelGenerator->DuplicateModel( ent->GetRenderEntity()->hModel, SeedClass.classname, true );
 				}
 			}
 		}
@@ -1802,6 +1804,9 @@ void Seed::ComputeEntityCount( void )
 			newNum = m_Classes[i].maxEntities;
 		}
 
+#ifdef M_DEBUG
+		gameLocal.Printf( "SEED %s: Entity count for class %i: %i.\n", GetName(), i, newNum );
+#endif
 		m_Classes[i].numEntities = newNum;
 		m_iNumEntities += newNum;
 	}
@@ -2265,14 +2270,10 @@ void Seed::PrepareEntities( void )
 	{
 		if (m_Classes[i].pseudo)
 		{
-			// remove the render model
-		   	if (m_Classes[i].hModel)
-			{
-				renderModelManager->FreeModel( m_Classes[i].hModel );
-				m_Classes[i].hModel = NULL;
-			}
+			// skip this class
 			continue;
 		}
+		// keep this class
 		newClasses.Append ( m_Classes[i] );
 	}
 	m_Classes.Swap( newClasses );		// copy over
@@ -2284,6 +2285,9 @@ void Seed::PrepareEntities( void )
 	{
 		ClassIndex.Append ( i );				// 1,2,3,...
 		m_Classes[i].seed = RandomSeed();		// random generator 2 inits the random generator 1
+#ifdef M_DEBUG
+		gameLocal.Printf("SEED %s: Using seed %i for class %i.\n", GetName(), m_Classes[i].seed, i );
+#endif
 	}
 
 	// Randomly shuffle all entries, but use the second generator for a "predictable" class sequence
@@ -3370,8 +3374,6 @@ void Seed::CombineEntities( void )
 			if (merged == 0)
 			{
 				PseudoClass.pseudo = true;
-				// PseudoClass.m_LOD = entityClass->m_LOD;
-	
 				if (entityClass->m_LOD)
 				{	
 					// Store m_LOD at the class
@@ -3411,7 +3413,7 @@ void Seed::CombineEntities( void )
 				PseudoClass.classname = FUNC_DUMMY;
 				// in case the combined model needs to be combined from multiple func_statics
 				PseudoClass.hModel = entityClass->hModel;
-				// not used here
+				// this is just a ptr, never free it
 				PseudoClass.spawnArgs = NULL;
 
 				// TODO: put this into m_LOD as there it can be shared and doesn't need to be
