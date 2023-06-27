ecf1d176496aaa48928ab54242f16335c4ad46bd
ordohereticus <contact@mastermindsintl.com>
2023-06-27 14:43:11 -0400

    Revision 5123

--- branches/d3gpl/game/actor.cpp
+++ branches/d3gpl/game/actor.cpp
@@ -1,18 +1,26 @@
-/***************************************************************************
- *
- * PROJECT: The Dark Mod
- * $Source$
- * $Revision: 5121 $
- * $Date: 2011-12-11 14:12:26 -0500 (Sun, 11 Dec 2011) $
- * $Author: greebo $
- *
- ***************************************************************************/
-
+/*****************************************************************************
+                    The Dark Mod GPL Source Code
+ 
+ This file is part of the The Dark Mod Source Code, originally based 
+ on the Doom 3 GPL Source Code as published in 2011.
+ 
+ The Dark Mod Source Code is free software: you can redistribute it 
+ and/or modify it under the terms of the GNU General Public License as 
+ published by the Free Software Foundation, either version 3 of the License, 
+ or (at your option) any later version. For details, see LICENSE.TXT.
+ 
+ Project: The Dark Mod (http://www.thedarkmod.com/)
+ 
+ $Revision: 5123 $ (Revision of last commit) 
+ $Date: 2011-12-12 01:37:40 -0500 (Mon, 12 Dec 2011) $ (Date of last commit)
+ $Author: greebo $ (Author of last commit)
+ 
+******************************************************************************/
 
 #include "../idlib/precompiled.h"
 #pragma hdrstop
 
-static bool init_version = FileVersionList("$Id: Actor.cpp 5121 2011-12-11 19:12:26Z greebo $", init_version);
+static bool init_version = FileVersionList("$Id: Actor.cpp 5123 2011-12-12 06:37:40Z greebo $", init_version);
 
 #include "Game_local.h"
 #include "DarkModGlobals.h"
