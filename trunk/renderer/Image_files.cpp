2198b882b97148281e3191fde9008412c2bc3a22
ordohereticus <contact@mastermindsintl.com>
2023-06-27 19:29:51 -0400

    Revision 5192

--- trunk/renderer/Image_files.cpp
+++ trunk/renderer/Image_files.cpp
@@ -11,8 +11,8 @@
  
  Project: The Dark Mod (http://www.thedarkmod.com/)
  
- $Revision: 5191 $ (Revision of last commit) 
- $Date: 2012-01-08 05:32:34 -0500 (Sun, 08 Jan 2012) $ (Date of last commit)
+ $Revision: 5192 $ (Revision of last commit) 
+ $Date: 2012-01-08 05:59:34 -0500 (Sun, 08 Jan 2012) $ (Date of last commit)
  $Author: greebo $ (Author of last commit)
  
 ******************************************************************************/
@@ -20,7 +20,7 @@
 #include "precompiled_engine.h"
 #pragma hdrstop
 
-static bool versioned = RegisterVersionedFile("$Id: Image_files.cpp 5191 2012-01-08 10:32:34Z greebo $");
+static bool versioned = RegisterVersionedFile("$Id: Image_files.cpp 5192 2012-01-08 10:59:34Z greebo $");
 
 #include "tr_local.h"
 
@@ -787,6 +787,50 @@ Interfaces with the huge libjpeg
 =========================================================
 */
 
+#if JPEG_LIB_VERSION < 80 && !defined(jpeg_mem_src)
+// greebo: jpeglib62 doesn't have jpeg_mem_src, so roll our own
+// taken directly from http://stackoverflow.com/questions/5280756/libjpeg-ver-6b-jpeg-stdio-src-vs-jpeg-mem-src
+
+/* Read JPEG image from a memory segment */
+static void init_source (j_decompress_ptr cinfo) {}
+static boolean fill_input_buffer (j_decompress_ptr cinfo)
+{
+    ERREXIT(cinfo, JERR_INPUT_EMPTY);
+return TRUE;
+}
+static void skip_input_data (j_decompress_ptr cinfo, long num_bytes)
+{
+    struct jpeg_source_mgr* src = (struct jpeg_source_mgr*) cinfo->src;
+
+    if (num_bytes > 0) {
+        src->next_input_byte += (size_t) num_bytes;
+        src->bytes_in_buffer -= (size_t) num_bytes;
+    }
+}
+static void term_source (j_decompress_ptr cinfo) {}
+static void jpeg_mem_src (j_decompress_ptr cinfo, void* buffer, long nbytes)
+{
+    struct jpeg_source_mgr* src;
+
+    if (cinfo->src == NULL) {   /* first time for this JPEG object? */
+        cinfo->src = (struct jpeg_source_mgr *)
+            (*cinfo->mem->alloc_small) ((j_common_ptr) cinfo, JPOOL_PERMANENT,
+            SIZEOF(struct jpeg_source_mgr));
+    }
+
+    src = (struct jpeg_source_mgr*) cinfo->src;
+    src->init_source = init_source;
+    src->fill_input_buffer = fill_input_buffer;
+    src->skip_input_data = skip_input_data;
+    src->resync_to_restart = jpeg_resync_to_restart; /* use default method */
+    src->term_source = term_source;
+    src->bytes_in_buffer = nbytes;
+    src->next_input_byte = (JOCTET*)buffer;
+}
+
+
+#endif
+
 /*
 =============
 LoadJPG
@@ -864,11 +908,7 @@ static void LoadJPG( const char *filename, unsigned char **pic, int *width, int 
 
   /* Step 2: specify data source (eg, a file) */
 
-#if JPEG_LIB_VERSION >= 80
   jpeg_mem_src(&cinfo, fbuffer, len);
-#else
-  jpeg_stdio_src(&cinfo, fbuffer);
-#endif
 
   /* Step 3: read file parameters with jpeg_read_header() */
 
