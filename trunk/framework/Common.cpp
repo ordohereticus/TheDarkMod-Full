642d34f8bec0ba189b42a71f2cf3ee1c1f99c5a8
ordohereticus <contact@mastermindsintl.com>
2023-06-27 16:37:43 -0400

    Revision 5149

--- trunk/framework/Common.cpp
+++ trunk/framework/Common.cpp
@@ -11,8 +11,8 @@
  
  Project: The Dark Mod (http://www.thedarkmod.com/)
  
- $Revision: 5147 $ (Revision of last commit) 
- $Date: 2012-01-02 13:21:57 -0500 (Mon, 02 Jan 2012) $ (Date of last commit)
+ $Revision: 5149 $ (Revision of last commit) 
+ $Date: 2012-01-02 14:05:23 -0500 (Mon, 02 Jan 2012) $ (Date of last commit)
  $Author: greebo $ (Author of last commit)
  
 ******************************************************************************/
@@ -3021,7 +3021,8 @@ void idCommonLocal::InitGame( void )
 	// force r_fullscreen 0 if running a tool
 	CheckToolMode();
 
-	idFile *file = fileSystem->OpenExplicitFileRead( fileSystem->RelativePathToOSPath( CONFIG_SPEC, "fs_savepath" ) );
+	// greebo: the config.spec file is saved to the mod save path in darkmod/fms/<fs_game>/
+	idFile *file = fileSystem->OpenExplicitFileRead( fileSystem->RelativePathToOSPath( CONFIG_SPEC, "fs_modSavepath" ) );
 	bool sysDetect = ( file == NULL );
 	if ( file ) {
 		fileSystem->CloseFile( file );
