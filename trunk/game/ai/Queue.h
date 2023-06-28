ce1b9f6c387066aaf803ad31800b04bd07bce9f8
ordohereticus <contact@mastermindsintl.com>
2023-06-28 18:00:23 -0400

    Revision 5474

--- trunk/game/ai/Queue.h
+++ trunk/game/ai/Queue.h
@@ -11,9 +11,9 @@
  
  Project: The Dark Mod (http://www.thedarkmod.com/)
  
- $Revision: 5121 $ (Revision of last commit) 
- $Date: 2011-12-11 14:12:26 -0500 (Sun, 11 Dec 2011) $ (Date of last commit)
- $Author: greebo $ (Author of last commit)
+ $Revision: 5474 $ (Revision of last commit) 
+ $Date: 2012-06-08 04:33:15 -0400 (Fri, 08 Jun 2012) $ (Date of last commit)
+ $Author: tels $ (Author of last commit)
  
 ******************************************************************************/
 
@@ -112,7 +112,7 @@ public:
 			element->Restore(savefile);
 
 			// Add the element to the queue
-			push_back(element);
+			this->push_back(element);
 		}
 	}
 };
