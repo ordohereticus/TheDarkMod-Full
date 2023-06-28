284586ac8be797fb425167242a7305b38bf90365
ordohereticus <contact@mastermindsintl.com>
2023-06-28 05:41:54 -0400

    Revision 5330

--- trunk/game/Entity.cpp
+++ trunk/game/Entity.cpp
@@ -11,15 +11,15 @@
  
  Project: The Dark Mod (http://www.thedarkmod.com/)
  
- $Revision: 5323 $ (Revision of last commit) 
- $Date: 2012-03-08 14:11:51 -0500 (Thu, 08 Mar 2012) $ (Date of last commit)
- $Author: tels $ (Author of last commit)
+ $Revision: 5330 $ (Revision of last commit) 
+ $Date: 2012-03-10 21:27:08 -0500 (Sat, 10 Mar 2012) $ (Date of last commit)
+ $Author: grayman $ (Author of last commit)
  
 ******************************************************************************/
 #include "precompiled_game.h"
 #pragma hdrstop
 
-static bool versioned = RegisterVersionedFile("$Id: Entity.cpp 5323 2012-03-08 19:11:51Z tels $");
+static bool versioned = RegisterVersionedFile("$Id: Entity.cpp 5330 2012-03-11 02:27:08Z grayman $");
 
 #pragma warning(disable : 4533 4800)
 
@@ -3155,12 +3155,12 @@ void idEntity::Show( void )
 {
 	// greebo: If the pre-hide clipmask is still uninitialised on Show(), the entity 
 	// has not been hidden before. Set this to something valid (i.e. the current clipmask)
-	if ( m_preHideClipMask == -1)
+	if ( m_preHideClipMask == -1 )
 	{
 		m_preHideClipMask = GetPhysics()->GetClipMask();
 	}
 
-	if ( m_preHideContents == -1)
+	if ( m_preHideContents == -1 )
 	{
 		m_preHideContents = GetPhysics()->GetContents();
 	}
@@ -3168,7 +3168,7 @@ void idEntity::Show( void )
 	if ( IsHidden() ) 
 	{
 		fl.hidden = false;
-		if( m_FrobBox && m_bFrobable )
+		if ( m_FrobBox && m_bFrobable )
 		{
 			m_FrobBox->SetContents( CONTENTS_FROBABLE );
 		}
@@ -3178,21 +3178,26 @@ void idEntity::Show( void )
 		idEntity *ent;
 		idEntity *next;
 
-		for( ent = GetNextTeamEntity(); ent != NULL; ent = next ) 
+		for ( ent = GetNextTeamEntity() ; ent != NULL ; ent = next ) 
 		{
 			next = ent->GetNextTeamEntity();
 			if ( ent->GetBindMaster() == this ) 
 			{
-				if (gameLocal.time >= ent->GetHideUntilTime()) // grayman #597 - one second needs to pass before showing
+				if ( gameLocal.time >= ent->GetHideUntilTime() ) // grayman #597 - one second needs to pass before showing
 				{
 					ent->Show();
 					if ( ent->IsType( idLight::Type ) )
 					{
-						static_cast<idLight *>( ent )->On();
+						static_cast<idLight *>(ent)->On();
 					}
 				}
 			}
 		}
+	}
+
+	if ( spawnArgs.GetBool("neverShow","0") ) // grayman #2998
+	{
+		PostEventMS( &EV_Hide, 0 ); // queue a hide for later
 	}
 }
 
@@ -10332,10 +10337,14 @@ void idEntity::ShowAttachmentInd( int ind, bool bShow )
 		goto Quit;
 	}
 
-	if( bShow )
+	if ( bShow )
+	{
 		ent->Show();
+	}
 	else
+	{
 		ent->Hide();
+	}
 
 Quit:
 	return;
